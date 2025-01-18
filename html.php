456c0c9be5d168a878362b508b92f1f8e9f4b8c9
justojaramillo <justojaramillo1991@gmail.com>
2025-01-17 21:53:09 -0500

    html class with php

--- html.php
+++ html.php
@@ -2,7 +2,13 @@
 
 class html{
     public static function doctype():void{
-        echo "<!DOCTYPE html>";
+        echo "<!DOCTYPE html>".PHP_EOL;
+    }
+    public static function footer($open = true):void{
+        echo $open ? "<footer>".PHP_EOL:"</footer>".PHP_EOL;
+    }
+    public static function div($open = true):void{
+        echo $open ? "<div>".PHP_EOL:"</div>".PHP_EOL;
     }
 }
 
---
+++ includes/header.php
@@ -0,0 +1,65 @@
+<?php
+require_once("html.php");
+?>
+
+<!-- Page Loader -->
+<div id="loader-wrapper">
+    <div id="loader"></div>
+    <div class="loader-section section-left"></div>
+    <div class="loader-section section-right"></div>
+<?php html::div(false); ?>
+<nav class="navbar navbar-expand-lg">
+    <div class="container">
+        <a class="navbar-brand" href="#">
+            <i class="fas fa-film mr-2"></i>
+            Catalog-Z
+        </a>
+        <button
+            class="navbar-toggler"
+            type="button"
+            data-toggle="collapse"
+            data-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+        >
+            <i class="fas fa-bars"></i>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav ml-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                    <a
+                    class="nav-link nav-link-1 active"
+                    aria-current="page"
+                    href="photos.php"
+                    >Photos</a
+                    >
+                </li>
+                <li class="nav-item">
+                    <a
+                    class="nav-link nav-link-1 active"
+                    aria-current="page"
+                    href="create.php"
+                    >create</a
+                    >
+                </li>
+                <li class="nav-item">
+                    <a
+                    class="nav-link nav-link-1 active"
+                    aria-current="page"
+                    href="register.php"
+                    >register</a
+                    >
+                </li>
+                <li class="nav-item">
+                    <a
+                    class="nav-link nav-link-1 active"
+                    aria-current="page"
+                    href="login.php"
+                    >login</a
+                    >
+                </li>
+            </ul>
+        <?php html::div(false); ?>
+    <?php html::div(false); ?>
+</nav>
